22d68ae612979437977d35ab9ea7de98e230b36a
Juan Gaspar García <24842781+PKGaspi@users.noreply.github.com>
2021-11-23 10:33:23 +0100

    Transformed into plugin
    Added class icons

---
+++ addons/AudioSequencePlayers/AudioSequencePlayersPlugin.gd
@@ -0,0 +1,8 @@
+tool
+extends EditorPlugin
+#
+#func _enter_tree() -> void:
+#	add_custom_type("AudioSegment", "Resource", preload("AudioSegmentResource.gd"), preload("icons/AudioSegmentResource.svg"))
+#
+#func _exit_tree() -> void:
+#	remove_custom_type("AudioSegment")
